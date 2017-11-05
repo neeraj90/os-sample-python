e35fc9e76243f20aa40767d9ea0dce16aa57dac9
goyaln <goyaln@amazon.com>
2017-11-05 23:01:51 +0530

    added support for static content

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask import send_from_directory
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "Hello World Neeraj!"
 
+@application.route('/static/<path:path>')
+def send_video(path):
+    return send_from_directory('static', path)
+
 if __name__ == "__main__":
+    application.debug = True
     application.run()
